6cdfa7ba04c4940caddb4f6546f164ea93b62f0c
Grace Alea Chandra <gcha0035@student.monash.edu>
2024-10-08 00:01:29 +1100

    added stacked bar chart vg.json
    need to re format and annotate

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -4,3 +4,6 @@ vegaEmbed("#map", vg_1).then(function(result) {
 var vg_2 = "production.vg.json";
 vegaEmbed("#area", vg_2).then(function(result) {
 }).catch(console.error);
+var vg_2 = "RegionProduction_bar.vg.json";
+vegaEmbed("#bar", vg_3).then(function(result) {
+}).catch(console.error);
