d20ea4691ee5a2b39f7b5a233ccd20550a9ee591
Grace Alea Chandra <gcha0035@student.monash.edu>
2024-10-14 00:39:57 +1100

    removed action dots in charts

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,9 @@
 var vg_1 = "australia_wineregions_map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, { actions: false }).then(function(result) {
 }).catch(console.error);
 var vg_2 = "production.vg.json";
-vegaEmbed("#area", vg_2).then(function(result) {
+vegaEmbed("#area", vg_2, { actions: false }).then(function(result) {
 }).catch(console.error);
 var vg_3 = "RegionProduction_bar.vg.json";
-vegaEmbed("#bar", vg_3).then(function(result) {
+vegaEmbed("#bar", vg_3, { actions: false }).then(function(result) {
 }).catch(console.error);
