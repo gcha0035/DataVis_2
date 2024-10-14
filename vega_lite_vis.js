1b950869053a98760e008f893eb1b0206842117e
Grace Alea Chandra <gcha0035@student.monash.edu>
2024-10-15 00:43:06 +1100

    upload another csv for destination markets of exports

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,11 +2,14 @@ var vg_1 = "australia_wineregions_map.vg.json";
 vegaEmbed("#map", vg_1, { actions: false }).then(function(result) {
 }).catch(console.error);
 var vg_2 = "production.vg.json";
-vegaEmbed("#area", vg_2, { actions: false }).then(function(result) {
+vegaEmbed("#line", vg_2, { actions: false }).then(function(result) {
 }).catch(console.error);
 var vg_3 = "RegionProduction_bar.vg.json";
 vegaEmbed("#bar", vg_3, { actions: false }).then(function(result) {
 }).catch(console.error);
 var vg_4 = "GrapeVariety_bar.vg.json";
 vegaEmbed("#bar2", vg_4, { actions: false }).then(function(result) {
+}).catch(console.error);
+var vg_5 = "ExportDest.vg.json";
+vegaEmbed("#pie", vg_5, { actions: false }).then(function(result) {
 }).catch(console.error);
